03912e0290c11895b048e2e716ddf1e502933387
Shuvo Roy <shuvoroysp@gmail.com>
2022-09-03 21:15:45 +0600

    Header section complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import CTA from './CTA'
 import './header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Shuvo Roy</h1>
+        <h5 className='text-light'>Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className='me'>
+          <img src={ME} alt="ME"/>
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
