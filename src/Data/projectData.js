b0260cf13a6f3a829365d066fed9a4b5ca9d1f89
Shuvo Roy <shuvoroysp@gmail.com>
2023-11-13 16:16:49 +0600

    update projects

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,11 +1,20 @@
 import hatbazar from '../Assests/HatBazar.png'
+import agencify from '../Assests/Agencify.png'
 export default [
     {
         id:1,
         title:'E-commerce Website',
-        desc:'Working as a front-end web developer for more than a year. I can convert any PSD or Figma file into a mobile or tab-responsive web application with clean code.',
+        desc:'Dynamic interfaces, elegant styling, strong backend functionality, and a safe shopping experience are all combined in this e-commerce site that was created with React, Tailwind, Node.js, MongoDB, and Stripe.',
         imgUrl:hatbazar,
         tech:['React','Node Js','MongoDB','Tailwind'],
         link:'https://hatbazar1.vercel.app/',
+    },
+    {
+        id:2,
+        title:'Agencify Landing Page',
+        desc:'A sleek showcase of agency services built with React for seamless interactivity and Tailwind for stylish responsiveness This dynamic combination results in a modern, modular, and user-friendly web experience.',
+        imgUrl:agencify,
+        tech:['React','Tailwind'],
+        link:'https://agencify.vercel.app/',
     },
 ]
